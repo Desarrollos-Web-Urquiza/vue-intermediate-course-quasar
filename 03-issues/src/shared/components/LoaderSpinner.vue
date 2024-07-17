550155eeedc18416742c384cefa56fd697239f59
Desarrollos Web Urquiza <desarrollos_web_urquiza@hotmail.com>
2024-07-16 23:57:22 -0300

    defining props

---
+++ 03-issues/src/shared/components/LoaderSpinner.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+// import { defineProps, withDefaults } from 'vue'
+
+interface Props {
+    color?: string,
+    size?: string,
+    thickness?: number,
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  color: 'primary',
+  size: '3rem',
+  thickness: 2
+})
+
+
+
+</script>
+
+<template>
+    <div class="column items-center">
+        <q-spinner :color="props.color" :size="props.size" :thickness="props.thickness" />
+        <span class="q-mt-sm">Loading</span>
+    </div>
+
+</template>
+
+
+<style scoped>
+
+</style>
