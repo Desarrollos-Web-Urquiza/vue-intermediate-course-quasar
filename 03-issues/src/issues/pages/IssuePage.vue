6b52cda0a6e5e7d9c5abf1630ef4063c71f79196
Desarrollos Web Urquiza <desarrollos_web_urquiza@hotmail.com>
2024-07-26 19:52:33 -0300

    generating IssueCards

--- 03-issues/src/issues/pages/IssuePage.vue
+++ 03-issues/src/issues/pages/IssuePage.vue
@@ -1,11 +1,31 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
+import LoaderSpinner from 'src/shared/components/LoaderSpinner.vue';
+
+const route = useRoute();
+const { id = ''} = route.params;
+console.log('id', id);
 
 </script>
 
 <template>
-    <div>
-        <span class="text-h2">Issue: </span>
-    </div>
+  <LoaderSpinner color="white" />
+
+  <router-link class="text-white" to="/" >Go back</router-link>
+
+  <!-- Header -->
+  <IssueCard />
+    
+  <!-- Comentarios -->
+  <LoaderSpinner :thickness="1" size="1.5rem" :show-text="false"  />
+
+  <div class="column">
+    <span class="text-h3 q-mb-md">Comments (5)</span>
+    <IssueCard v-for="comment of 5" :key="comment" />
+
+  </div>
+
+
 </template>
 
 
